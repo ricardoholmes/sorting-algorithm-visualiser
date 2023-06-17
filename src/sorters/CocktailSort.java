9ee905ead14ed7ab3a77d6f500f018789d7e86a0
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-06-17 13:00:42 +0100

    Created cocktail sort file

---
+++ src/sorters/CocktailSort.java
@@ -0,0 +1,15 @@
+package sorters;
+
+public class CocktailSort extends Sorter {
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public void sort(int delay) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+}
