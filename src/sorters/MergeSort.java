eaa8efe0fa83bc97a1b444b4fcf76d5f80fed79d
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-06-20 12:55:36 +0100

    Implemented merge sort

--- src/sorters/MergeSort.java
+++ src/sorters/MergeSort.java
@@ -8,7 +8,36 @@ public class MergeSort extends Sorter {
 
     @Override
     public void sort(int delay, boolean sortAscending) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+        recursiveSort(0, model.getArrayLength(), delay, sortAscending);
+    }
+
+    // interval [start, end)
+    void recursiveSort(int start, int end, int delay, boolean sortAscending) {
+        if (start + 1 == end) {
+            return;
+        }
+
+        int middle = start + ((end - start) / 2);
+
+        recursiveSort(start, middle, delay, sortAscending);
+        recursiveSort(middle, end, delay, sortAscending);
+
+        int leftIndex = start;
+        int rightIndex = middle;
+
+        while (leftIndex < rightIndex && rightIndex < end) {
+            int leftNum = controller.getNumAtIndex(leftIndex);
+            int rightNum = controller.getNumAtIndex(rightIndex);
+            if (inOrder(rightNum, leftNum, sortAscending)) {
+                controller.moveNumber(rightIndex, leftIndex);
+                rightIndex++;
+                leftIndex++;
+
+                sleep(delay);
+            }
+            else {
+                leftIndex++;
+            }
+        }
     }
 }
