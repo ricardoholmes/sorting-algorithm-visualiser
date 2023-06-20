183d4b77a364f0fbceb595f20356370df81005a4
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-06-20 12:26:31 +0100

    Created merge sort file

---
+++ src/sorters/MergeSort.java
@@ -0,0 +1,15 @@
+package sorters;
+
+public class MergeSort extends Sorter {
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public void sort(int delay, boolean sortAscending) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+}
